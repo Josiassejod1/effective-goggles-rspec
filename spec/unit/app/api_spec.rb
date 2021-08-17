8237dfefa763007a9fb2a628e6bf586118a18dd5
Dalvin <Dalvin.sejour@dutchie.com>
2021-08-17 12:56:06 -0700

    clean up api

--- spec/unit/app/api_spec.rb
+++ spec/unit/app/api_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../../../app/api'
 require 'rack/test'
-require  'active_support/core_ext/hash/conversions'
+
 module ExpenseTracker
   RSpec.describe API do
     include Rack::Test::Methods
@@ -55,24 +55,23 @@ module ExpenseTracker
 
     describe 'POST /expenses' do
       before do
-          allow(ledger).to receive(:record)
-            .with(expense)
-            .and_return(RecordResult.new(true, 417, nil))
+        allow(ledger).to receive(:record)
+          .with(expense)
+          .and_return(RecordResult.new(true, 417, nil))
       end
 
-        context 'when the expense is successfully recorded' do
-          it 'returns the expense id' do
-            post '/expenses', JSON.generate(expense)
-            parsed = response_body
-            expect(parsed).to include('expense_id' => 417)
-          end
-
-
-          it 'responds with a 200 (OK)' do
-            post '/expenses', JSON.generate(expense)
-            expect(last_response.status).to eq(200)
-          end
+      context 'when the expense is successfully recorded' do
+        it 'returns the expense id' do
+          post '/expenses', JSON.generate(expense)
+          parsed = response_body
+          expect(parsed).to include('expense_id' => 417)
         end
+
+        it 'responds with a 200 (OK)' do
+          post '/expenses', JSON.generate(expense)
+          expect(last_response.status).to eq(200)
+        end
+      end
 
 
       context 'when the expense fails validation' do
